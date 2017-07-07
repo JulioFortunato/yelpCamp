f1d2604886b84e1ecd169e4ff339707370b035f4
Felipe Soares <felipeluizsoares@gmail.com>
2017-07-06 23:08:05 -0300

    add a new campground using the database

--- app.js
+++ app.js
@@ -18,27 +18,27 @@ const campgroundSchema = new mongoose.Schema({
 
 const Campground = mongoose.model('Campground', campgroundSchema)
 
-Campground.create({
-  name: 'Salmon',
-  image: 'http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx'
-}, (error, campground) => {
-     if (error) {
-        return console.log(error)
-      }
-
-      console.log(campground)
-   })
-
-Campground.create({
-  name: 'Granite',
-  image: 'http://www.fondulacpark.com/wp-content/uploads/2015/01/campground-pic-1.jpg'
-}, (error, campground) => {
-     if (error) {
-        return console.log(error)
-      }
-
-      console.log(campground)
-   })
+// Campground.create({
+//   name: 'Salmon',
+//   image: 'http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx'
+// }, (error, campground) => {
+//      if (error) {
+//         return console.log(error)
+//       }
+//
+//       console.log(campground)
+//    })
+//
+// Campground.create({
+//   name: 'Granite',
+//   image: 'http://www.fondulacpark.com/wp-content/uploads/2015/01/campground-pic-1.jpg'
+// }, (error, campground) => {
+//      if (error) {
+//         return console.log(error)
+//       }
+//
+//       console.log(campground)
+//    })
 
 app.get('/', (req, res) => {
   res.render('landing')
@@ -56,11 +56,14 @@ app.get('/campgrounds', (req, res) => {
 
 app.post('/campgrounds', (req, res) => {
   const { name, image } = req.body
-  const campground = { name, image }
 
-  campgrounds = [ ...campgrounds, campground ]
+  Campground.create({ name, image }, (error, campground) => {
+    if (error) {
+      return console.log(error)
+    }
 
-  res.redirect('/campgrounds')
+    res.redirect('/campgrounds')
+  })
 })
 
 app.get('/campgrounds/new', (req, res) => {
