8fdb516d960ab474790c3800746efb6192a8fcb5
Felipe Soares <felipeluizsoares@gmail.com>
2017-07-06 23:01:16 -0300

    create campgrounds on the database

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect('mongodb://localhost/yelp_camp', { useMongoClient: true })
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
-let campgrounds = [
-  { name: 'Salmon', image: 'http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx' },
-  { name: 'Granite', image: 'http://www.fondulacpark.com/wp-content/uploads/2015/01/campground-pic-1.jpg' },
-  { name: 'Mountain', image: 'https://img.hipcamp.com/image/upload/c_limit,f_auto,h_1200,q_60,w_1920/v1440478008/campground-photos/csnhvxn0qcki2id5vxnc.jpg' }
-]
+//schema
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+})
+
+const Campground = mongoose.model('Campground', campgroundSchema)
+
+Campground.create({
+  name: 'Salmon',
+  image: 'http://www.nationalparks.nsw.gov.au/~/media/DF58734103EF43669F1005AF8B668209.ashx'
+}, (error, campground) => {
+     if (error) {
+        return console.log(error)
+      }
+
+      console.log(campground)
+   })
+
+Campground.create({
+  name: 'Granite',
+  image: 'http://www.fondulacpark.com/wp-content/uploads/2015/01/campground-pic-1.jpg'
+}, (error, campground) => {
+     if (error) {
+        return console.log(error)
+      }
+
+      console.log(campground)
+   })
 
 app.get('/', (req, res) => {
   res.render('landing')
 })
 
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', { campgrounds })
+  Campground.find({}, (error, campgrounds) => {
+    if (error) {
+      return console.log(error)
+    }
+
+    res.render('campgrounds', { campgrounds })
+  })
 })
 
 app.post('/campgrounds', (req, res) => {
