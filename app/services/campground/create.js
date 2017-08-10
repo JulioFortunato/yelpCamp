1bd908fbaafc8d8e836bc28af88ca39619445cb9
Felipe Soares <felipeluizsoares@gmail.com>
2017-08-09 21:38:11 -0300

    add campground service to create a campground

---
+++ app/services/campground/create.js
@@ -0,0 +1,16 @@
+const campgroundsModel = require('../../models/campground')
+const CreateCampgroundsService = {
+  perform (campground) {
+    return new Promise((resolve, reject) => {
+      campgroundsModel.create(campground)
+        .then((campgroundAdded) => {
+          return resolve(campgroundAdded)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  }
+}
+
+module.exports = CreateCampgroundsService
