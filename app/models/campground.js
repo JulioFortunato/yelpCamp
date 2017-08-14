b1911f832cb4f26039f81bcad11eef5a9a327a08
Felipe Soares <felipeluizsoares@gmail.com>
2017-08-14 13:23:32 -0300

    add validations to campground model

--- app/models/campground.js
+++ app/models/campground.js
@@ -1,9 +1,14 @@
 const { mongoose, mongoSchema } = require('../db/connect')
 
+
+const nameRequired = [true, 'The campground must have a name']
+const imageRequired = [true, 'The campground must have an image']
+const descriptionRequired = [true, 'The campground must have a description']
+
 const campgroundSchema = new mongoSchema({
-  name: String,
-  image: String,
-  description: String,
+  name: { type: String, trim: true, required: nameRequired },
+  image: { type: String, trim: true, required: imageRequired },
+  description: { type: String, trim: true, required: descriptionRequired},
   comments: [
     {
       type: mongoose.Schema.Types.ObjectId,
