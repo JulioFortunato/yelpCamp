7d94bc0f44325a3e5cad5b0ef835d9a9c39ef56a
Felipe Soares <felipeluizsoares@gmail.com>
2017-08-14 13:24:07 -0300

    add validations to comment model

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,8 +1,10 @@
 const { mongoose, mongoSchema } = require('../db/connect')
+const textRequired = [ true, 'The comment should have a text']
+const authorRequired = [ true, 'The comment should have an author']
 
 const commentSchema = new mongoose.Schema({
-  text: String,
-  author: String,
+  text: { type: String, required: textRequired },
+  author: { type: String, required: authorRequired },
 })
 
 module.exports = mongoose.model('Comment', commentSchema)
